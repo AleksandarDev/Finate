8f44ba87a88cf8780a52c7a489a2d461ff77c8fc
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2016-06-10 00:11:17 +0200

    Graph

---
+++ Finate.UWP/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Finate.Models;
+using Prism.Windows.Mvvm;
+
+namespace Finate.UWP.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+        public ObservableCollection<Transaction> WeeklyExpenses { get; } = new ObservableCollection<Transaction>()
+        {
+            new Transaction()
+            {
+                Amount = 80,
+                Date = new DateTime(2016, 6, 2)
+            },
+            new Transaction()
+            {
+                Amount = 180,
+                Date = new DateTime(2016, 6, 3)
+            },
+            new Transaction()
+            {
+                Amount = 120,
+                Date = new DateTime(2016, 6, 4)
+            },
+            new Transaction()
+            {
+                Amount = 90,
+                Date = new DateTime(2016, 6, 5)
+            },
+            new Transaction()
+            {
+                Amount = 70,
+                Date = new DateTime(2016, 6, 6)
+            },
+            new Transaction()
+            {
+                Amount = 140,
+                Date = new DateTime(2016, 6, 7)
+            },
+            new Transaction()
+            {
+                Amount = 140,
+                Date = new DateTime(2016, 6, 7)
+            }
+        };
+    }
+}
--- Finate.UWP/Views/HomePage.xaml.cs
+++ Finate.UWP/Views/HomePage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Finate.UWP.Annotations;
+using Finate.UWP.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -7,11 +12,27 @@ namespace Finate.UWP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HomePage
+    public sealed partial class HomePage : INotifyPropertyChanged
     {
         public HomePage()
         {
             this.InitializeComponent();
+            this.DataContextChanged += this.HomePageDataContextChanged;
+        }
+
+        private void HomePageDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            this.OnPropertyChanged(nameof(ConcreteDataContext));
+        }
+
+        public HomePageViewModel ConcreteDataContext => this.DataContext as HomePageViewModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
